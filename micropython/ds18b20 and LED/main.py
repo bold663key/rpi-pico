d254ab5c3452abce259b1ead859dc83aa02b68cd
bold663key <93290181+bold663key@users.noreply.github.com>
2023-04-09 22:02:45 +0200

    Add files via upload

---
+++ micropython/ds18b20 and LED/main.py
@@ -0,0 +1,97 @@
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor, pico_led, LED
+import machine, onewire, ds18x20
+from machine import Pin
+
+ssid = 'rpi'
+password = 'samochody1R'
+
+led = LED(13)
+
+ds_pin = machine.Pin(2)
+ 
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+ 
+roms = ds_sensor.scan()
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        pico_led.on()
+        sleep(0.5)
+        pico_led.off()
+        sleep(0.5)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    led.on()
+    sleep(2)
+    led.off()
+    return ip
+
+
+def open_socket(ip):
+    # Open a socket
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    return connection
+
+def webpage(temperature, state):
+    #Template HTML
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+            <form action="./lighton">
+            <input type="submit" value="Dioda ON" />
+            </form>
+            <form action="./lightoff">
+            <input type="submit" value="Dioda OFF" />
+            </form>
+            <p>Dioda jest {state}</p>
+            <p>Temperature is {temperature}</p>
+            </body>
+            </html>
+            """
+    return str(html)
+
+def serve(connection):
+    #Start a web server
+    state = 'OFF'
+    led.off()
+    pico_led.off()
+    temperature = 0
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        try:
+            request = request.split()[1]
+        except IndexError:
+            pass
+        if request == '/lighton?':
+            led.on()
+            state = 'ON'
+        elif request =='/lightoff?':
+            led.off()
+            state = 'OFF'
+        for rom in roms:
+            ds_sensor.convert_temp()
+            sleep(1)
+            temperature = (ds_sensor.read_temp(rom))
+            
+        html = webpage(temperature, state)
+        client.send(html)
+        client.close()
+try:
+    ip = connect()
+    connection = open_socket(ip)
+    serve(connection)
+except KeyboardInterrupt:
+    machine.reset()
